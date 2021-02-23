b79d2d03a24639c34673e3cd79cb09d4befbce02
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-02-23 15:37:13 -0500

    Adds APP

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,2 +1,10 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
+const firstName = getInput(0);
+const lastName = getInput(1);
+const fullName = firstName + ' ' + lastName;
+
+console.log('Good evening ' + fullName);
+console.log('Your capitalized name is ' + fullName.toLocaleUpperCase());
+console.log('Your initials are '+ firstName[0] + '.' + lastName[0] + '.');
+console.log('Your codeimersives email is ' + firstName.toLowerCase() + '.' + lastName.toLowerCase() + '@codeimmersives.com');
